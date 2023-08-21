15a420d4b21ca7fb7b738ac78ddf82cff8c78521
Haris Qureshi <harisq705@gmail.com>
2023-08-21 15:13:42 +0500

    first commit

---
+++ react-weather-app-master/src/modules/CityComponent.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import React from "react";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SearchBox = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;  // Changed from space-evenly to center
+  margin-top: 20px;
+  border: black solid 1px;
+  border-radius: 2px;
+
+  & input {
+    padding: 10px;
+    font-size: 14px;
+    border: none;
+    outline: none;
+    font-family: Montserrat;
+    font-weight: bold;
+  }
+  
+  & button {
+    background-color: black;
+    font-size: 14px;
+    padding: 0 10px;
+    color: white;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-weight: bold;
+  }
+`;
+
+const ChooseCityLabel = styled.span`
+  color: black;
+  margin: 10px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const WelcomeWeatherLogo = styled.img`
+  width: 140px;
+  height: 140px;
+  margin: 20px auto;  // Reduced margin
+`;
+
+const CityComponent = (props) => {
+  const { updateCity, fetchWeather } = props;
+  return (
+    <Container>
+      <SearchBox onSubmit={fetchWeather}>
+        <input
+          onChange={(e) => updateCity(e.target.value)}
+          placeholder="City"
+        />
+        <button type="submit">Search</button>
+      </SearchBox>
+      <WelcomeWeatherLogo src="/react-weather-app/icons/perfect-day.svg" />
+      <ChooseCityLabel>Find Weather of your city</ChooseCityLabel>
+    </Container>
+  );
+};
+
+export default CityComponent;
